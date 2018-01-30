fc252035bc784762996cdf093afac30129fdb5c6
Krzysztof Nowicki <krzysztof11nowicki@gmail.com>
2018-01-30 23:59:05 +0100

    main > main slider

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
 $(document).ready(function(){
 
+  let next = $("#nextPicture");
+  let prev = $("#prevPicture");
+  let list = $(".slider > ul").children();
+  let counter = 0;
 
+  list[counter].classList.add("visible")
+
+  next.click(goNext);
+  prev.click(goPrev);
+
+  function goNext() {
+
+      console.log("goNext");
+      list[counter].classList.remove("visible");
+      if (counter < list.length-1) {
+        counter++;
+      }
+      list[counter].classList.add("visible");
+  };
+
+  function goPrev() {
+      console.log("goPrev");
+      list[counter].classList.remove("visible");
+      if (counter > 0) {
+        counter--;
+      }
+      list[counter].classList.add("visible");
+  };
 
 
 }); /*doc.ready.func END*/
