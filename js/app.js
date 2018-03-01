990c668c10ee0603186caf922538960837e273a4
Krzysztof Nowicki <krzysztof11nowicki@gmail.com>
2018-03-01 19:21:47 +0100

    rwd menu

--- js/app.js
+++ js/app.js
@@ -28,26 +28,10 @@ $(document).ready(function(){
         }
     }
 })
-// end of main slider by OwlCarousel2
-
-// smooth scrolling
-  $('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash;
-	    var $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-// end smooth scrolling
-
+//END main slider by OwlCarousel2
 //ordering items
 
-var shopingList = [];
+let shopingList = [];
 $('.gallery-photo').on('click', function (e){
   e.preventDefault();
 
@@ -57,14 +41,71 @@ $('.gallery-photo').on('click', function (e){
     return shopingList;
   };
 
-  var item = $(this).data().item;
+  let item = $(this).data().item;
   adToList(item)
 
     $('.contact-form--textarea').val("Hej, Zamawiam następujące towary:" + '\n' + " " + shopingList.join(" "));
     $('.contact-form--textarea').html($('.contact-form--textarea').text().replace(/\n\r?/g, '<br/>'));
 })
 
-//end of ordering items
+//END of ordering items
+// smooth scrolling
+  $('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
 
+	    let target = this.hash;
+	    let $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+//END smooth scrolling
+// menu <=> mobile menu
+var menu = document.getElementById("drop");
+var hamb = document.getElementById("hamburger");
+// change MENU <-> full menu
+var mobile = window.matchMedia("(max-width:760px)");
+var flipMenu = function(mobile) {
+  if (mobile.matches) {
+    menu.style.display = "none";
+    hamb.style.display = "block";
+  }
+  else {
+    menu.style.display = "flex";
+    hamb.style.display = "none";
+  }
+}
+
+mobile.addListener(function(mobile){
+  if (mobile.matches) {
+    menu.style.display = "none";
+    hamb.style.display = "block";
+  }
+  else {
+    menu.style.display = "flex";
+    menu.style.marginLeft = "0";
+    hamb.style.display = "none";
+  }
+});
+
+flipMenu(mobile);
+//dropdown list look
+hamb.addEventListener("click", openList);
+
+function openList() {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  }else{
+    menu.style.display = "block";
+    menu.style.marginLeft = "20%";
+    menu.style.position = "absolute";
+  }
+};
+
+
+//END menu <=> mobile menu
 
 }); /*doc.ready.func END*/
